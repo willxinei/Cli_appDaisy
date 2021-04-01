23df374cf9921aa20333a943b2eed87668fae16b
william <will30barbosa@yahoo.com>
2021-04-01 06:06:51 -0300

    adicionando linear

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {View} from 'react-native';
 
-const App: React.FC = () => <View />;
+const App: React.FC = () => (
+  <View style={{flex: 1}}
+)
 
 export default App;
