784c55f0d89e9ee58d6cda6bed794f842402f015
william <will30barbosa@yahoo.com>
2021-04-01 06:47:04 -0300

    linear ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import {View} from 'react-native';
+import {StatusBar, View} from 'react-native';
+import Linear from 'react-native-linear-gradient';
 
-const App: React.FC = () => (
-  <View style={{flex: 1}}
-)
+const App: React.FC = () => {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#E4C6D5" />
+      <Linear style={{flex: 1}} colors={['#E4C6D5', '#EAEAEA']}>
+        <View />
+      </Linear>
+    </>
+  );
+};
 
 export default App;
