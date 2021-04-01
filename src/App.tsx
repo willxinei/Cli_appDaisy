1e40c094a97720efb5e2a446e9a123261baeab35
william <will30barbosa@yahoo.com>
2021-04-01 19:12:23 -0300

    import de react-nvigation

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {StatusBar, View} from 'react-native';
+import Linear from 'react-native-linear-gradient';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#E4C6D5" />
+      <Linear style={{flex: 1}} colors={['#E4C6D5', '#EAEAEA']}>
+        <View />
+      </Linear>
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/SiginIn/indes.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Container} from './styles';
+
+const SiginIn: React.FC = () => {
+  return <Container />;
+};
+
+export default SiginIn;
